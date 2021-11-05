c3d59988f77f5d0a57a33a6eef3aa010c0a93e04
SergioPereiraMega <proagua2@gmail.com>
2021-11-05 09:49:03 -0300

    Text about y centrado

--- resources/views/sections/quienes_somos.blade.php
+++ resources/views/sections/quienes_somos.blade.php
@@ -1,16 +1,18 @@
 <div class="container about">
     <div class="row">
         <div class="col-sm-12">
-
-            <dl>
-                <dt>Agronegocios San Francisco nacio en 2019,</dt>
-                <dd>fue creada por 2 personas que han trabajado durante varios años en empresas multinacionales en el area de grains and oilseeds desde el trading hasta la administracion general.</dd>
-
-                <dt>La empresa tiene dos grandes areas: </dt>
-                <dd>Una es la compra, venta, importacion y exportacion de todo lo que sean granos, cereales y oleaginosas, buscando la maxima eficiencia y beneficio para las partes que intervienen en el negocio.</dd>
-
-                <dd>La otra area es brindarle a empresas comerciales proveedoras de insumos la posibilidad de administrar y gestionar el negocio de granos como una forma de busqueda de eficiencia y elminacion de costos fijos (tercerizacion). Esto es un formato novedoso que no existe en la actualidad. ASF logro formar un servicio nuevo adaptado en las necesidades de muchas empresas comerciales posibilitandole a estas obtener mas y mejores beneficios sin invertir tiempo ni capital humano, por ende disminuyendo costos y aumentando la eficiencia.</dd>
-            </dl>
+            <h2>Agronegocios San Francisco nacio en 2019,</h2>
+            <p>fue creada por 2 personas que han trabajado durante varios años en empresas multinacionales en el area de
+                grains and oilseeds desde el trading hasta la administracion general.</p>
+            <h3>La empresa tiene dos grandes areas:</h3>
+            <p>Una es la compra, venta, importacion y exportacion de todo lo que sean granos, cereales y oleaginosas,
+                buscando la maxima eficiencia y beneficio para las partes que intervienen en el negocio.</p>
+            <p>La otra area es brindarle a empresas comerciales proveedoras de insumos la posibilidad de administrar y
+                gestionar el negocio de granos como una forma de busqueda de eficiencia y elminacion de costos fijos
+                (tercerizacion). Esto es un formato novedoso que no existe en la actualidad. ASF logro formar un
+                servicio nuevo adaptado en las necesidades de muchas empresas comerciales posibilitandole a estas
+                obtener mas y mejores beneficios sin invertir tiempo ni capital humano, por ende disminuyendo costos y
+                aumentando la eficiencia.</p>
 
         </div>
     </div>
